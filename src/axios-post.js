d9b97bf6263222eae6cbf00005abcf0dba2920f8
hoyipyik <hoyipyik@protonmail.com>
2021-08-08 16:34:35 +0800

    change DB to Europe

--- src/axios-post.js
+++ src/axios-post.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL: "https://chatapp-hyy-default-rtdb.asia-southeast1.firebasedatabase.app/"
+    baseURL: "https://todo-online-hyy-default-rtdb.europe-west1.firebasedatabase.app/"
 })
 
 export default instance
