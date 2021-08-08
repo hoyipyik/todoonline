6ef88edda9091817876bfe76f425ede375c17854
hoyipyik <hoyipyik@protonmail.com>
2021-08-08 17:45:08 +0800

    add property colour

--- src/Components/Item/Item.js
+++ src/Components/Item/Item.js
@@ -1,20 +1,40 @@
 import React from 'react'
+import { blue, green } from '@material-ui/core/colors'
+// import { withStyles } from '@material-ui/core/styles'
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles'
 
 import Checkbox from '@material-ui/core/Checkbox'
 import DeleteIcon from '@material-ui/icons/Delete'
 import Grid from '@material-ui/core/Grid'
-  
+
+
 
 export default function item (props){
+
+    const checkboxTheme = createMuiTheme({
+        palette: {
+          primary:{
+            main: blue[300],
+            },
+          secondary: {
+            main: blue[700],
+          },
+          default:{
+            main: green[300],
+          }
+        },
+      })
     // const classes = useStyles()
     return (
         <div >
             <Grid container spacing={3}>
                 <Grid item xs={10}>
+                <ThemeProvider theme={checkboxTheme}>
                     <Checkbox 
                         onClick={()=>props.checkboxHandler(props.id)}
-                        color="primary"
+                        color={!props.property?"primary":"secondary"}
                         checked={props.checked}/>
+                 </ThemeProvider>
                     <span 
                         onClick={()=>props.propertyHandler(props.id)}
                         style={{ textDecoration: props.checked ? "line-through" : null,
