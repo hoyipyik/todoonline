5e60c5494776bb24cf4927ceffe0a50be56ad78c
hoyipyik <hoyipyik@protonmail.com>
2021-08-10 18:30:29 +0800

    update description

--- src/axios-post.js
+++ src/axios-post.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL: "https://todo-online-hyy-default-rtdb.europe-west1.firebasedatabase.app/private"
+    baseURL: "https://todo-online-hyy-default-rtdb.europe-west1.firebasedatabase.app/public"
 })
 
 export default instance
